6ec0facb63e35299a208ff8aee08f2c726202850
Shane Rosse <rosse@usc.edu>
2017-10-08 17:42:36 -0700

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome!</h1>
-        </header>
-        <p className="App-intro">
-          <p>My name is Shane Rosse.</p>
-          <p>I'm an avid learner of new technology. </p>
-          <p>This page was built with Create React App. </p>
-          <p>Follow along as I built out the rest of my portfolio!</p>
-        </p>
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome!</h1>
+        <h2 className="nav-btn-container">
+          <button className="nav-btn" onClick={() => {}}>About Me</button>
+          <button className="nav-btn" onClick={() => {}}>Projects</button>
+          <button className="nav-btn" onClick={() => {}}>Contact</button>
+        </h2>
+      </header>
+      <div className="App-intro">
+        <div className="Intro-body">My name is Shane Rosse.</div>
+        <div className="Intro-body">I'm an avid learner of new technology. </div>
+        <div className="Intro-body">This page was built with Create React App. </div>
+        <div className="Intro-body">Follow along as I built out the rest of my portfolio!</div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
